879b19ee384bc78c773f980fbb93cbbf52f7178f
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-09-13 22:35:45 -0500

    "Added new dependencies to package-lock.json and package.json, updated src/app/page.tsx to include PassKeyModal component, and modified src/lib/actions/appointment.actions.ts to include new functions and logic."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 
-export default function Home() {
+export default function Home({searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true'
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PassKeyModal/>}
+      
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
